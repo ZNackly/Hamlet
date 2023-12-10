0c51799169f4eb81855fa38e826c774e64bca4ab
Dan <137257813+ZNackly@users.noreply.github.com>
2023-12-10 12:00:53 +0500

    Create Hamlet.cpp

---
+++ Hamlet.cpp
@@ -0,0 +1,6 @@
+cout << "The quote from \"Hamlet\":"s << endl;
+cout << "To be, or not to be, that is the question:"s << endl;
+cout << "Whether \'tis nobler in the mind to suffer"s << endl;
+cout << "The slings and arrows of outrageous fortune,"s << endl;
+cout << "Or to take arms against a sea of troubles"s << endl;
+cout << "And by opposing end them."s << endl;
